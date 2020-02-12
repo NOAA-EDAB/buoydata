01d3ee82a02ea0c2e4aea445fd885f1ac3c88ebd
andybeet <22455149+andybeet@users.noreply.github.com>
2020-02-12 15:59:21 -0500

    Added lazy data describing all buoys - added documention for data and package - added script in data-raw to extract list of buoys

---
+++ R/nbdcbuoy.R
@@ -0,0 +1,15 @@
+#' nbdcbuoy: Extract buoy data from nbdc
+#'
+#'Tools to aid in extraction of data from the nbdc website
+#'
+#'\itemize{
+#'\item Get_ functions to get data.
+#'\item functions to manipulate the data, aggregate the data
+#'}
+#'
+#'To learn more about using \code{nbdcbuoy} click the index below
+#'
+#'
+#' @docType package
+#' @name nbdcbuoy
+NULL
