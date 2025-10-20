a06bdbf79f8c20804a15bd84c75525c28a957fb2
andybeet <22455149+andybeet@users.noreply.github.com>
2025-10-20 17:45:44 -0400

    update buoy world data set doc

--- R/buoyDataWorld.R
+++ R/buoyDataWorld.R
@@ -1,24 +1,28 @@
-#' buoyDataWorld: List of stations
+#' buoyDataWorld: NDBC Standard Meteorological Buoy Data, 1970-present
 #'
-#' A complete list of buoys listed on ndbc website along with buoy specific features such as location and data range.
+#' A complete list of buoys listed on ERDDAP along with buoy specific features such as location and data range.
 #'
-#' @format A data frame of several elements
+#' @format A data frame
 #' \describe{
 #'     \item{ID}{Buoy ID name}
 #'     \item{Y1}{First year of data}
 #'     \item{YN}{Last year of data}
 #'     \item{nYEARS}{Number of years of data}
+#'     \item{lastMeasurement}{Date/Time of the last recorded measurement}
 #'     \item{LAT}{Latitude}
 #'     \item{LON}{Longitude}
 #'     \item{STATION_LOC}{Description of station location}
-#'     \item{STATION_NAME}{Name of Station}
 #'     \item{TTYPE}{Number of years of data}
 #'     \item{TIMEZONE}{Time Zone}
 #'     \item{OWNER}{Code assigned to OWNERNAME}
 #'     \item{OWNERNAME}{Owner organization of data }
-#'     \item{COUNTRYCODE}{Country code associated with OWNERNAME}
+#'     \item{COUNTRY}{Country code associated with OWNERNAME}
+#'     \item{HULL}{}
+#'     \item{PAYLOAD}{}
+#'     \item{FORCAST}{}
+#'     \item{NOTE}{NDBC notes regarding buoy}
 #'}
 #'
 #'
-#'@source National Data Buoy Center: \url{ndbc.noaa.gov}
+#'@source ERDDAP: \url{https://coastwatch.pfeg.noaa.gov/erddap/tabledap/cwwcNDBCMet.html}. This data is pulled from National Data Buoy Center: \url{ndbc.noaa.gov} on a cron job
 "buoyDataWorld"
