07c9372ec702d68dd8c0f7851392a254a7acccc3
andybeet <22455149+andybeet@users.noreply.github.com>
2025-10-21 14:09:14 -0400

    air formatting

--- data-raw/compare_data.r
+++ data-raw/compare_data.r
@@ -2,36 +2,34 @@
 #
 
 compare_data <- function() {
-
   current <- readRDS(here::here("data-raw/tempData.rds"))
   new <- readRDS(here::here("data-raw/newData.rds"))
   #new <- readRDS(here::here("data-raw/testdata.rds"))
 
-  sameDim <- all.equal(dim(current),dim(new))
+  sameDim <- all.equal(dim(current), dim(new))
 
   # look for changes in some of the columns
-  cols <- c("ID","Y1","YN", "nYEARS")
+  cols <- c("ID", "Y1", "YN", "nYEARS")
   for (aname in cols) {
-    bool <- all.equal(current[[aname]],new[[aname]])
+    bool <- all.equal(current[[aname]], new[[aname]])
     #message(paste0("Same ",aname," = ",bool))
   }
 
   # compare just these columns
   cc <- current |> dplyr::select(dplyr::all_of(cols))
   nn <- new |> dplyr::select(dplyr::all_of(cols)) |> dplyr::as_tibble()
-  sameMain <- all.equal(nn,cc)
+  sameMain <- all.equal(nn, cc)
 
   # return if good
 
-  aa <- dplyr::setdiff(current,new)
-  bb <- dplyr::setdiff(new,current) |> dplyr::as_tibble()
+  aa <- dplyr::setdiff(current, new)
+  bb <- dplyr::setdiff(new, current) |> dplyr::as_tibble()
 
   df <- NULL
   # loop through all stations and find differences
   stations <- aa$ID
   for (aid in stations) {
     for (aname in names(aa)) {
-
       # print(c(aid,aname))
       row <- list()
       aaa <- aa |> dplyr::filter(ID == aid)
@@ -44,45 +42,38 @@ compare_data <- function() {
       if (is.na(aaa[[aname]]) & is.na(bbb[[aname]])) {
         next
       }
-      if (any(is.na(aaa[[aname]]),is.na(bbb[[aname]]))) {
-        row <- c(aid,aname,aaa[[aname]],bbb[[aname]])
+      if (any(is.na(aaa[[aname]]), is.na(bbb[[aname]]))) {
+        row <- c(aid, aname, aaa[[aname]], bbb[[aname]])
         # print(aid)
         # print(aaa[[aname]] )
         # print(bbb[[aname]] )
         #print("33333")
-        df <- rbind(df,row)
+        df <- rbind(df, row)
         next
       }
 
-
       if (aaa[[aname]] != bbb[[aname]]) {
-        if (aname %in% c("LAT","LON")) {
-          if (abs(aaa[[aname]]-bbb[[aname]]) > .001) {
-            row <- c(aid,aname,aaa[[aname]],bbb[[aname]])
+        if (aname %in% c("LAT", "LON")) {
+          if (abs(aaa[[aname]] - bbb[[aname]]) > .001) {
+            row <- c(aid, aname, aaa[[aname]], bbb[[aname]])
             # print(aid)
             # print(abs(aaa[aname]-bbb[aname]))
             # print("11111")
-            df <- rbind(df,row)
+            df <- rbind(df, row)
           }
-
         } else {
-          row <- c(aid,aname,aaa[[aname]],bbb[[aname]])
+          row <- c(aid, aname, aaa[[aname]], bbb[[aname]])
           # print(aid)
           # print(aaa[[aname]] )
           # print(bbb[[aname]] )
           #print("22222")
-          df <- rbind(df,row)
+          df <- rbind(df, row)
         }
       }
-
-
     }
-
   }
-  df <- data.frame(df,row.names = NULL)
-  names(df) <- c("ID","Var","Current","New")
+  df <- data.frame(df, row.names = NULL)
+  names(df) <- c("ID", "Var", "Current", "New")
 
-
-  return(list(df=df,sameDim=sameDim,sameMain=sameMain,mainVars = cols))
-
+  return(list(df = df, sameDim = sameDim, sameMain = sameMain, mainVars = cols))
 }
